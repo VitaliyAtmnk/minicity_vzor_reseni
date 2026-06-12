e8b6831fbd7bfebe24967d2ee6b50643684943c7
mst2 <matej.strejc@infis.cz>
2026-06-12 11:27:25 +0200

    commit

--- src/city/ConsoleApplication.java
+++ src/city/ConsoleApplication.java
@@ -2,8 +2,17 @@ package city;
 
 import city.logic.CityManager;
 import city.logic.CityMap;
+import city.logic.Statistics;
 import city.tiles.Tile;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -35,10 +44,133 @@ public class ConsoleApplication {
      * @param args argumenty příkazové řádky se nepoužívají
      */
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            CityManager cityManager = new CityManager(new CityMap(DEFAULT_MAP_SIZE), DEFAULT_BUDGET);
-            new ConsoleApplication(scanner, cityManager).run();
+
+        CityMap cityMap = new CityMap(5);
+        CityManager cityManager = new CityManager(cityMap, 5000);
+
+        String path = "inputs";
+
+        List<String> commands = new ArrayList<>();
+        List<File> validFiles = nactiSoubory(path);
+
+        for (File file : validFiles) {
+            try {
+                commands.addAll(Files.readAllLines(file.toPath()));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
+
+        ConsoleApplication app =
+                new ConsoleApplication(new Scanner(System.in), cityManager);
+
+        for (String command : commands) {
+            app.processCommand(command);
+        }
+        System.out.println(commands);
+
+        List<String> export = new ArrayList<>();
+        export.add("MINICITY REPORT");
+        Statistics statistics = new Statistics(cityManager);
+
+        export.add("\nTOP 3 Tiles: \n");
+
+        for (Tile tile : statistics.getTopThreeTiles()) {
+            export.add(tile.getDescription());
+        }
+
+
+        export.add("\nProducing tiles: \n");
+        export.add("Producing tiles: "
+                + statistics.getProducingPercentage() + "%");
+
+
+        export.add("\nTotal population: \n");
+        export.add(String.valueOf(statistics.getTotalPopulation()));
+        export.add("\nOccupancy: \n");
+        export.add(statistics.getOccupancyPercentage() + "%");
+
+        export.add("\nTiles by type:\n");
+        statistics.getTileCountByType()
+                .forEach((type, count) ->
+                        export.add(type + ": " + count));
+
+        System.out.println(export);
+
+
+            try {
+                FileWriter writer = new FileWriter("./output/report.txt");
+                for(String str: export) {
+                    writer.write(str + System.lineSeparator());
+                }
+                writer.close();
+            } catch (IOException e) {
+                System.out.println("Chyba při zapisování: " + e.getMessage());
+            }
+
+        List<String> csv = new ArrayList<>();
+
+        csv.add("x;y;type;symbol;score;distanceFromCenter;createdAt");
+
+        List<Tile> tiles = cityManager.getTiles();
+
+        for (Tile tile : tiles) {
+
+            int distance = tile.getCoordinate()
+                    .distanceTo(cityMap.getCenter());
+
+            csv.add(
+                    tile.getCoordinate().getX() + ";" +
+                            tile.getCoordinate().getY() + ";" +
+                            tile.getClass().getSimpleName() + ";" +
+                            tile.getSymbol() + ";" +
+                            tile.getScore() + ";" +
+                            distance + ";" +
+                            tile.getCreatedAt()
+            );
+        }
+
+        try {
+            FileWriter writer = new FileWriter("./output/tiles.csv");
+
+            for (String line : csv) {
+                writer.write(line + System.lineSeparator());
+            }
+
+            writer.close();
+
+        } catch (IOException e) {
+            System.out.println("Chyba při zapisování CSV: "
+                    + e.getMessage());
+        }
+
+
+
+
+
+    }
+
+    public static List<File> nactiSoubory(String path) {
+        File dir = new File(path);
+        File[] soubory = dir.listFiles();
+
+        List<File> vysledek = new ArrayList<>();
+
+        if (soubory == null) {
+            return vysledek;
+        }
+
+        for (File f : soubory) {
+            if (f.isFile()
+                    && f.getName().startsWith("city_")
+                    && f.getName().endsWith(".txt")
+                    && f.length() <= 1024) {
+
+                vysledek.add(f);
+            }
+        }
+
+        return vysledek;
     }
 
     /**
@@ -68,13 +200,13 @@ public class ConsoleApplication {
 
         String[] parts = trimmedCommand.split(" ");
         switch (parts[0].toLowerCase()) {
-            case "add" ->       addTile(parts);
-            case "remove" ->    removeTile(parts);
-            case "info" ->      printInfo(parts);
-            case "upgrade" ->   upgradeTile(parts);
-            case "next" ->      cityManager.nextTurn();
-            case "show" ->      cityManager.printMap();
-            case "budget" ->    System.out.println(cityManager.getBudget());
+            case "add" -> addTile(parts);
+            case "remove" -> removeTile(parts);
+            case "info" -> printInfo(parts);
+            case "upgrade" -> upgradeTile(parts);
+            case "next" -> cityManager.nextTurn();
+            case "show" -> cityManager.printMap();
+            case "budget" -> System.out.println(cityManager.getBudget());
             case "exit" -> {
                 System.out.println("Score: " + cityManager.getTotalScore());
                 running = false;
